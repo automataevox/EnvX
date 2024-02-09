6d5878ac8d35ca1be3231b95a92a7c6e31d773ef
Automata - WinX <musicbyblakk@gmail.com>
2024-02-09 23:44:38 +0100

    added loop of changing status

--- onReady.js
+++ onReady.js
@@ -1,21 +1,33 @@
-const ip = require("ip");
+const fs = require('fs');
 const { Events, ChannelType, ActivityType } = require('discord.js');
+const statusesData = fs.readFileSync('statuses.json');
+const statuses = JSON.parse(statusesData);
 
 module.exports = async (client, channels) => {
   client.once(Events.ClientReady, async () => {
-    console.log("DEV MODE ENABLED!\n");    
-    client.user.setPresence({
-      activities: [
-        {
-          name: "We will take over the world!",
-          type: ActivityType.Custom,
-        },
-      ],
-      status: "dnd",
-    });
+    console.log("DEV MODE ENABLED!\n");
+    let index = 0;
     
+    setPresence(statuses[index]);
+
+   
+    setInterval(() => {
+        index = (index + 1) % statuses.length;
+        setPresence(statuses[index]);
+    }, 15000);
+
     channels = await getChannelIDs();
-    
+
+    function setPresence(status) {
+      client.user.setPresence({
+        activities: [{
+          name: status,
+          type: ActivityType.Custom
+        }],
+        status: 'dnd'
+      });
+    }
+
     function getChannelIDs() {
       var array = [];
       let channels = client.channels.cache;
